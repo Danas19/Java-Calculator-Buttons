147d47eb820fb0aea0af1e06ac74e0264e6e14ee
Danas <danas.mikelionis@gmail.com>
2019-02-23 20:13:54 +0200

    Added 1 button to main. //to pass to buttons fraim so that buttons could access frame object, maybe will be a button that will create another calculator

--- src/com/vtvpmc/DanasMikelionis/Application.java
+++ src/com/vtvpmc/DanasMikelionis/Application.java
@@ -1,7 +1,15 @@
 package com.vtvpmc.DanasMikelionis;
 
+import javax.swing.JButton;
+
+import com.vtvpmc.DanasMikelionis.buttons.AbstractButton;
+import com.vtvpmc.DanasMikelionis.buttons.Button0;
+
 public class Application {
 	public static void main(String[] args) {
 		Frame frame = new Frame();
+		
+		AbstractButton[] buttons = new AbstractButton[20];
+		buttons[0] = (AbstractButton)new Button0();
 	}
 }
--- src/com/vtvpmc/DanasMikelionis/Frame.java
+++ src/com/vtvpmc/DanasMikelionis/Frame.java
@@ -1,11 +1,9 @@
 package com.vtvpmc.DanasMikelionis;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import com.vtvpmc.DanasMikelionis.buttons.AbstractButton;
-import com.vtvpmc.DanasMikelionis.buttons.Button0;
 
 public class Frame {
 	JFrame frameObject;
@@ -15,14 +13,7 @@ public class Frame {
 		this.frameObject.setVisible(true);
 		this.frameObject.setSize(400, 400);
 		
-		AbstractButton[] abstractButtons = new AbstractButton[20];
 		
-		abstractButtons[0] = (AbstractButton)new Button0();
-		JPanel panel = new JPanel();
-		panel.setLayout(null);
-		panel.add(abstractButtons[0].getButton());
-		frameObject.getContentPane().add(panel);
-		//frameObject.add(panel);
 	}
 	
 	public Frame(String title) {
@@ -30,7 +21,11 @@ public class Frame {
 		this.frameObject.setTitle(title);
 	}
 	
-	public void addButtons(JButton[] buttons) {
+	public void addButtons(AbstractButton[] buttons) {
 		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.add(buttons[0].getButton());
+		frameObject.getContentPane().add(panel);
 	}
 }
