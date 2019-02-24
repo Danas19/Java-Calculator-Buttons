95df1740b2d0687b6745532e145bb5882fd520b1
Danas <danas.mikelionis@gmail.com>
2019-02-24 18:21:13 +0200

    Changed Action. Removed Bug(no longer Exception/s after equals sign) but will maybe change what equals sign do //to make it more like most calculators

--- src/com/vtvpmc/DanasMikelionis/Action.java
+++ src/com/vtvpmc/DanasMikelionis/Action.java
@@ -1,9 +1,10 @@
 package com.vtvpmc.DanasMikelionis;
 
 public class Action {
+
 	public static void doActionEquals(Frame frame) {
 			String answer = frame.getCalculationLabelText();
-			if (frame.getCalculationLabelText().matches(".{0,}[+-X:].{0,}")) {
+			if (indexOf(frame.getCalculationLabelText()) != -1) {
 				int indexOfSign = indexOf(frame.getCalculationLabelText());
 				String number1 = frame.getCalculationLabelText()
 						.substring(0, indexOfSign - 1);
@@ -14,7 +15,6 @@ public class Action {
 					if (indexOfSign != answer.length() - 2) {
 						number2 = answer
 								.substring(indexOfSign + 2, answer.length());
-						System.out.println(number2);
 						answer = String.valueOf(Double.valueOf(number1)
 								+ Double.valueOf(number2));
 					} else {
@@ -24,12 +24,13 @@ public class Action {
 					break;
 				}
 				
+				frame.setCaluculationLabelText(answer);
 			}
-			frame.setCaluculationLabelText(answer);
+			
 	}
 	
 	public static void doActionArithmetic(Frame frame, String whatButton) {
-		if (frame.getCalculationLabelText().matches(".{0,}[X:+-].{0,}")) {
+		if (indexOf(frame.getCalculationLabelText()) != -1) {
 			doActionEquals(frame);
 		}
 		frame.setCaluculationLabelText(frame.getCalculationLabelText()
